d91a898381bd8cabb81572fce26be95a152240bc
Ling Xu <36904516+xulingggg@users.noreply.github.com>
2020-11-30 17:36:20 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FundamentalsOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 256; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    continue;
+                }
+                else if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+            }
+
+        }
+    }
+}
+
